948165487c7fbffc6a40c2c651d27b306fbbc600
Radek Zlebcik <radek.zlebcik@desy.de>
2020-07-01 15:02:21 +0200

    Small update

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ fifo="/tmp/zlebcr/hepmc.fifo"
 rm -f  $fifo
 mkfifo $fifo
 
-./main41 $fifo 10 &
-rivet --pwd -a  bjetsNew $fifo
+./main41 test.hepmc 10 #&
+#./main41 $fifo 10 &
+#rivet --pwd -a  ATLAS_2016_CONF_2016_092  $fifo
